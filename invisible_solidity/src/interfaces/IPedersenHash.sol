ec0a54ace25dcff1d5c2c13225c67021ed92df4b
Snojj25 <snoj.jure1@gmail.com>
2023-09-29 14:22:02 +0200

    spliting modules

--- invisible_solidity/src/interfaces/IPedersenHash.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-interface IPedersenHash {
-    function hash(
-        bytes memory input
-    ) external view returns (uint256[] memory output);
-}
